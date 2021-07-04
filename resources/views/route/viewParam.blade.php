8c2c24ae174480a7a689520e4137aff410a04f97
Andrzej Szelka <szelkaandrzej@gmail.com>
2021-07-05 00:07:12 +0200

    Exercies with routes

---
+++ resources/views/route/viewParam.blade.php
@@ -0,0 +1,4 @@
+<ul>
+    <li>Param 1: {{$param1}}</li>
+    <li>Name: {{$name}}</li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,55 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/goodbye/{name}', function (string $name) {
+    return 'Goodbaye: ' . $name;
+});
+
+/* Route::get('/example', function () {
+    return 'here is method get';
+}); */
+
+$uri = '/example';
+Route::get($uri, fn () => 'hello here is get with arrow function');
+Route::post($uri, fn () => 'hello here is post with arrow function');
+Route::put($uri, fn () => 'hello here is put with arrow function');
+Route::patch($uri, fn () => 'hello here is patch with arrow function');
+Route::delete($uri, fn () => 'hello here is delete with arrow function');
+Route::options($uri, fn () => 'hello here is options with arrow function');
+
+//dla wybranych metod
+Route::match(['get', 'post'], '/match', fn () => 'here is match for get and post with arrow function');
+
+//wszystkie metody
+Route::any('/all', fn () => 'all methods');
+
+//pominięcie callback function, direct redirect to view
+Route::view('/view/route', 'route.view');
+Route::view(
+    '/view/route/var1',
+    'route.viewParam',
+    [
+        'param1' => 'var1 - it is our route',
+        'name' => 'Andrew'
+    ]
+);
+
+Route::get('posts/{postId}/title/{title}', function (int $postId, string $title) {
+    var_dump($title);
+    dd($postId);
+});
+
+Route::get('users/{nick?}', function (string $nick = null) {
+    dd($nick);
+});
+
+// domyślna wartość
+Route::get('users-default-value/{nick?}', function (string $nick = 'Andrew') {
+    dd($nick);
+})->where(['nick' => '[a-z]+']);
+
+//wyrażenie regularne, np. tylko a-z
+Route::get('users-regexp/{nick}', function (string $nick) {
+    dd($nick);
+})->where(['nick' => '[a-z]+']);
